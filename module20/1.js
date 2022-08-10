805ea88399fd659158ea8f8beb6734761f5cc7b5
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-10 10:00:24 +0000

    convertHourToMinutes

---
+++ module20/1.js
@@ -0,0 +1,10 @@
+// Practice Problem 1
+// Write a function that will take hour as the input parameter and will
+// convert it into minutes and will return the result in minutes.
+
+function convertHourToMinutes(hour) {
+  return hour * 60;
+}
+
+console.log(convertHourToMinutes(1)); // 60
+console.log(convertHourToMinutes(2)); // 120
