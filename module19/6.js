5dd022f85a8681b6f2f82b6183d633ec6e35020f
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-10 09:35:45 +0000

    multiplicationTable

---
+++ module19/6.js
@@ -0,0 +1,9 @@
+// ১. একটা ফাংশন লিখবা যেটা ১৩ এর নামতা (multiplication table) আউটপুট হিসেবে দেখাবে।
+
+function multiplicationTable(num) {
+  for (var i = 1; i <= 10; i++) {
+    console.log(num + " x " + i + " = " + num * i);
+  }
+}
+
+multiplicationTable(13);
